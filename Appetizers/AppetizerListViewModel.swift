87bf1f6ba97f45b95c41aa98a55586985997cbea
lutzzdias <thiagolutzdias@hotmail.com>
2023-12-07 20:52:55 +0000

    refactor: move logic from view to viewModel

---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Thiago Lütz Dias on 07/12/23.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
