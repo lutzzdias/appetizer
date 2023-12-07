46dd52523b6d2d5d03e135e62dd6fb271e9e24ea
lutzzdias <thiagolutzdias@hotmail.com>
2023-12-07 23:29:02 +0000

    feat: form validation and move logic to viewModel

--- Appetizers/Screens/AccountView/AccountViewModel.swift
+++ Appetizers/Screens/AccountView/AccountViewModel.swift
@@ -5,4 +5,36 @@
 //  Created by Thiago Lütz Dias on 07/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+final class AccountViewModel: ObservableObject {
+    @Published var firstName = ""
+    @Published var lastName = ""
+    @Published var email = ""
+    @Published var birthdate = Date()
+    
+    @Published var extraNapkins = false
+    @Published var frequentRefills = false
+    
+    @Published var alertItem: AlertItem?
+    
+    var isValidForm: Bool {
+        guard !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty else { 
+            alertItem = AlertContext.invalidForm
+            return false
+        }
+        
+        guard email.isValidEmail else {
+            alertItem = AlertContext.invalidEmail
+            return false
+        }
+        
+        return true
+    }
+    
+    func saveChanges() {
+        guard isValidForm else { return }
+        
+        print("Changes saved successfully")
+    }
+}
