178f6369581888f6cc464616567dbf3cdda1da27
lutzzdias <thiagolutzdias@hotmail.com>
2023-12-07 19:18:30 +0000

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Thiago Lütz Dias on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
