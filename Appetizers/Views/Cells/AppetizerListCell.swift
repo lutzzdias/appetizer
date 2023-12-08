8dfe42025ad873486ff69725732d030ddf4ec924
lutzzdias <thiagolutzdias@hotmail.com>
2023-12-08 02:28:42 +0000

    feat: use asyncImage in listCell (asyncImage does not allow caching)

--- Appetizers/Views/Cells/AppetizerListCell.swift
+++ Appetizers/Views/Cells/AppetizerListCell.swift
@@ -13,10 +13,16 @@ struct AppetizerListCell: View {
     
     var body: some View {
         HStack {
-            AppetizerRemoteImage(urlString: appetizer.imageURL)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 120, height: 90)
-                .cornerRadius(8)
+            
+            AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+                image
+                    .standardImageStyle()
+            } placeholder: {
+                Image("food-placeholder")
+                    .standardImageStyle()
+                    
+            }
+
             
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
