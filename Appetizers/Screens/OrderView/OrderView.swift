4eccb98c9ccf763a4b163eda5ce460622f4571f1
lutzzdias <thiagolutzdias@hotmail.com>
2023-12-08 00:15:53 +0000

    feat: implement swipe to delete

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct OrderView: View {
     
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationView {
             VStack {
                 List {
-                    ForEach(MockData.orderItems) { appetizer in
+                    ForEach(orderItems) { appetizer in
                         AppetizerListCell(appetizer: appetizer)
                     }
+                    .onDelete(perform: deleteItem)
                 }
                 .listStyle(.plain)
                 
@@ -39,6 +42,10 @@ struct OrderView: View {
         }
         
         return price
+    }
+    
+    func deleteItem(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
