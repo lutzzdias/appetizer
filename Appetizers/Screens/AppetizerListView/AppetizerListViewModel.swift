764dabed4a70a76b8e7e7748f92e4be883e84619
lutzzdias <thiagolutzdias@hotmail.com>
2023-12-08 02:05:24 +0000

    refactor: use async await for API requests

--- Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@MainActor
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
@@ -17,28 +18,13 @@ final class AppetizerListViewModel: ObservableObject {
     
     func getAppetizers() {
         isLoading = true
-        NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async { [self] in
-                isLoading = false
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                    
-                case .failure(let error):
-                    switch error {
-                    case .invalidResponse:
-                        alertItem = AlertContext.invalidResponse
-                    
-                    case .invalidURL:
-                        alertItem = AlertContext.invalidURL
-                    
-                    case .invalidData:
-                        alertItem = AlertContext.invalidData
-                    
-                    case .unableToComplete:
-                        alertItem = AlertContext.unableToComplete
-                    }
-                }
+        
+        Task {
+            do {
+                appetizers = try await NetworkManager.shared.getAppetizers()
+                do { isLoading = false }
+            } catch {
+                alertItem = AlertContext.unableToComplete
             }
         }
     }
--- Appetizers/Utilities/Managers/NetworkManager.swift
+++ Appetizers/Utilities/Managers/NetworkManager.swift
@@ -17,38 +17,16 @@ final class NetworkManager {
     
     private init() {}
     
-    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
-        guard let url = URL(string: appetizerURL) else {
-            completed(.failure(.invalidURL))
-            return
-        }
+    func getAppetizers() async throws -> [Appetizer] {
+        guard let url = URL(string: appetizerURL) else { throw APError.invalidURL }
         
-        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            if let _ = error {
-                completed(.failure(.unableToComplete))
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(.failure(.invalidResponse))
-                return
-            }
-            
-            guard let data = data else {
-                completed(.failure(.invalidData))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
-                completed(.success(decodedResponse.request))
-            } catch {
-                completed(.failure(.invalidData))
-            }
-        }
+        let (data, _) = try await URLSession.shared.data(from: url)
         
-        task.resume()
+        do {
+            let decoder = JSONDecoder()
+            let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
+            return decodedResponse.request
+        } catch { throw APError.invalidData }
     }
     
     
